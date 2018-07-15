a18fa99dfa47f196c435a3b02bcfa65653cd8a3d
dg-mfb <dgmfb73@gmail.com>
2018-07-15 14:00:22 +0100

    adding .net core web client

---
+++ Web/WebNetCoreClient/Controllers/ValuesController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebNetCoreClient.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        // GET api/values
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            string key = "401b09eab3c013d4ca54922bb802bec8fd50a75f201d8b3727429090fb337591abd3e44453b954555b7a0812e1081c39b740293f765eae731f5a65ed1";
+            var symmetricSecurityKey = new Microsoft.IdentityModel.Tokens.SymmetricSecurityKey(Encoding.UTF8.GetBytes(key));
+            var signingCredentials = new Microsoft.IdentityModel.Tokens.SigningCredentials(symmetricSecurityKey, Microsoft.IdentityModel.Tokens.SecurityAlgorithms.HmacSha256Signature);
+            var jwtSecurityTokenHandler = new JwtSecurityTokenHandler();
+            var tokenFromString = Request.Query["token"];
+            if (tokenFromString.Count > 0)
+            {
+                var token = jwtSecurityTokenHandler.ReadToken(tokenFromString.First());
+            }
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
--- WebApi/Token/SSOAuthorizationServerProvider.cs
+++ WebApi/Token/SSOAuthorizationServerProvider.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.IdentityModel.Tokens;
 using System.IdentityModel.Tokens.Jwt;
+using System.Text;
 using System.Threading.Tasks;
 using Kernel.Authorisation;
 using Kernel.DependancyResolver;
 using Kernel.Initialisation;
 using Kernel.Security.CertificateManagement;
+using Microsoft.IdentityModel.Tokens;
 using SSOOwinMiddleware.Contexts;
 
 namespace WebApi.Token
@@ -22,8 +24,7 @@ namespace WebApi.Token
             var sSOTokenEndpointResponseContext = context as SSOTokenEndpointResponseContext;
             if (sSOTokenEndpointResponseContext != null)
             {
-                var jth = new JwtSecurityTokenHandler();
-                
+                string key = "401b09eab3c013d4ca54922bb802bec8fd50a75f201d8b3727429090fb337591abd3e44453b954555b7a0812e1081c39b740293f765eae731f5a65ed1";
                 var identity = sSOTokenEndpointResponseContext.Ticket.Identity;
                 var properties = sSOTokenEndpointResponseContext.Ticket.Properties;
                 var resolver = ApplicationConfiguration.Instance.DependencyResolver;
@@ -31,9 +32,14 @@ namespace WebApi.Token
                 var certContext = new X509CertificateContext { StoreName = "My", StoreLocation = System.Security.Cryptography.X509Certificates.StoreLocation.LocalMachine };
                 certContext.SearchCriteria.Add(new CertificateSearchCriteria { SearchCriteriaType = System.Security.Cryptography.X509Certificates.X509FindType.FindBySubjectName, SearchValue = "www.eca-international.com" });
                 var certificate = certManager.GetCertificateFromContext(certContext);
-                var jwt = jth.CreateEncodedJwt("https://www.glasswall-dev.com", "https://www.glasswall-dev.com", identity, properties.IssuedUtc.GetValueOrDefault().DateTime, properties.ExpiresUtc.GetValueOrDefault().DateTime, properties.IssuedUtc.GetValueOrDefault().DateTime , new Microsoft.IdentityModel.Tokens.SigningCredentials(new Microsoft.IdentityModel.Tokens.X509SecurityKey(certificate), SecurityAlgorithms.RsaSha256Signature), null);
+                var x509SecurityKey = new X509SecurityKey(certificate);
+                var symmetricSecurityKey = new Microsoft.IdentityModel.Tokens.SymmetricSecurityKey(Encoding.UTF8.GetBytes(key));
+                //var signingCredentials = new Microsoft.IdentityModel.Tokens.SigningCredentials(x509SecurityKey, Microsoft.IdentityModel.Tokens.SecurityAlgorithms.RsaSha256Signature);
+                var signingCredentials = new Microsoft.IdentityModel.Tokens.SigningCredentials(symmetricSecurityKey, Microsoft.IdentityModel.Tokens.SecurityAlgorithms.HmacSha256Signature);
+                var jwtSecurityTokenHandler = new JwtSecurityTokenHandler();
+                var jwt = jwtSecurityTokenHandler.CreateEncodedJwt("https://www.glasswall-dev.com", "https://www.glasswall-dev.com", identity, properties.IssuedUtc.GetValueOrDefault().DateTime, properties.ExpiresUtc.GetValueOrDefault().DateTime, properties.IssuedUtc.GetValueOrDefault().DateTime , signingCredentials, null);
                 var redirectUri = sSOTokenEndpointResponseContext.Configuration.TokenResponseUrl;
-                var uri = String.Format("{0}?token={1}", redirectUri.AbsoluteUri, jwt);
+                var uri = String.Format("{0}api/values?token={1}", redirectUri.AbsoluteUri, jwt);
                 sSOTokenEndpointResponseContext.Response.Redirect(uri);
                 context.RequestCompleted();
             }
